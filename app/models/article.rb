80f934007da36d4445d537e999ee1823ec0220b5
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-01-06 11:48:28 -0600

    Add dependent destroy to comments from artcile

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
-  has_many :comments, inverse_of: :article
+  has_many :comments, inverse_of: :article, dependent: :destroy
   validates :title, :content, presence: true
 end
