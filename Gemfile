4d29e60aee49113e6e46ff6c9a1218cfa9482ee2
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-01-11 11:55:56 -0600

    Implement PostGIS active record adapter

--- Gemfile
+++ Gemfile
@@ -5,10 +5,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.0"
 
+gem "activerecord-postgis-adapter", "~> 8.0"
 gem "jbuilder"
+gem "pg", "~> 1.1"
 gem "puma", "~> 5.0"
 gem "rails", "~> 7.0.1"
-gem "pg", "~> 1.1"
 
 gem "importmap-rails"
 gem "sprockets-rails"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_151328) do
+ActiveRecord::Schema.define(version: 2022_01_11_174003) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_11_02_151328) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.text "content"
     t.string "auhtor"
     t.datetime "created_at", precision: 6, null: false
